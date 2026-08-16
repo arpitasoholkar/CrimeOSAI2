068f4a5e97e9dafd80723aef43b84426724d96c0
Arpita Soholkar <arpitasoholkar@gmail.com>
2026-08-16 18:12:49 +0530

    s

--- crimeos-frontend/vite.config.js
+++ crimeos-frontend/vite.config.js
@@ -1,9 +1,24 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: 5173
+//   }
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173
-  }
+    port: 5173,
+    headers: {
+      'Cross-Origin-Opener-Policy': 'unsafe-none',
+      'Cross-Origin-Embedder-Policy': 'unsafe-none',
+    },
+  },
 })
